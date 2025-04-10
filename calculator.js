d7acdace01de099d644c6cd4618f5596520a4892
desham sai prasad goud <dgoud@msystechnologies.com>
2025-04-10 12:54:37 +0530

    Add support for custom delimiters

--- calculator.js
+++ calculator.js
@@ -4,7 +4,16 @@ function add(numbers) {
     }
 
     let delimiter = /[,\n]/; // Default delimiters: comma and newline
-    let numbersArray = numbers.split(delimiter).map(num => {
+    let numbersString = numbers;
+
+    if (numbers.startsWith('//')) {
+        const delimiterEndIndex = numbers.indexOf('\n');
+        const customDelimiter = numbers.substring(2, delimiterEndIndex);
+        delimiter = new RegExp(`[${customDelimiter},\n]`);
+        numbersString = numbers.substring(delimiterEndIndex + 1);
+    }
+
+    const numbersArray = numbersString.split(delimiter).map(num => {
         const parsed = parseInt(num, 10);
         return isNaN(parsed) ? 0 : parsed;
     });
