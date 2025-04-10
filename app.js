8b7421b3f6c5bc2e641a5ad922695ac6c3609717
desham sai prasad goud <dgoud@msystechnologies.com>
2025-04-10 10:48:24 +0530

    Select input, button, and result DOM elements

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const calculateBtn = document.getElementById('calculate');
+    const numbersInput = document.getElementById('numbers');
+    const resultDiv = document.getElementById('result');
+}
